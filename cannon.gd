5093b8a970e45f92a1de13b2b3b92e05ab01648b
owenbgriffin-collab <owenbgriffin@gmail.com>
2026-09-03 16:42:51 -0400

    made the power and angle more readable

--- cannon.gd
+++ cannon.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Fire"):
 		var ball :=preload("res://Ball.tscn").instantiate()
-		add_child(ball)
+		add_sibling(ball)
+		ball.global_position = global_position
 		ball.apply_impulse(Vector2(1,-1)*500)
